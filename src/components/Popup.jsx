d3aef534a0b3135e0ccd391ee7956d80fe7ea2d1
thiyagarajanp98 <thiyagarajanp98@gmail.com>
2024-10-05 01:42:32 +0530

    search result page working

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,6 +1,6 @@
 import React, { useRef, useEffect, useState } from 'react';
 import './Popup.css';
-import { fetchAutocompleteData } from '../redux/apiSlice';
+import { clearAutocompleteData, fetchAutocompleteData } from '../redux/apiSlice';
 import { useDispatch, useSelector } from 'react-redux';
 import Loading from './Loading';
 const Popup = ({ isOpen, closePopup }) => {
@@ -19,6 +19,9 @@ const Popup = ({ isOpen, closePopup }) => {
         // Mock search functionality - filtering based on input
         if (value.trim()) {
             dispatch(fetchAutocompleteData(value));
+        }
+        if(!value){
+            dispatch(clearAutocompleteData());
         }
     };
 
@@ -63,147 +66,33 @@ const Popup = ({ isOpen, closePopup }) => {
                 </div>
                 {loading && <Loading />}
                 {error && <p>Error: {error}</p>}
-                {autocompleteData != null ? (
-
-
-                    // Object.keys(autocompleteData).map((key) => (
-                    //   <div className="suggestions-box">
-                    //     {autocompleteData[key].data.map((suggestion, index) => (
-                    //       <div key={index} className="suggestion-item">
-                    //         <img src={suggestion.image} alt={suggestion.title} className="suggestion-image" />
-                    //         <span className="suggestion-title">{suggestion.title}</span>
-                    //       </div>
-                    //     ))}
-                    //   </div>
-                    // ))
+                {autocompleteData != null ?
                     <div className="suggestions-container">
-                        <div className="suggestions-box">
-                            <div className="suggestions-header">
-                                <h2>Top Result</h2>
-                                <button className="view-all-button">View All</button>
-                            </div>
-                            <div className="suggestions-card">
-                                <img src="https://c.saavncdn.com/415/Leo-Original-Motion-Picture-Soundtrack-English-2023-20231019170311-150x150.jpg" alt="Leo (Original Motion Picture Soundtrack)" />
-                                <span><p>Leo (Original Motion Picture Soundtrack)</p>
-                                    <p>Tamil Album · 2023 · Anirudh Ravichander</p></span>
-                            </div>
-                            <div className="suggestions-card">
-                                <img src="https://c.saavncdn.com/046/Leo-Telugu-Original-Motion-Picture-Soundtrack-English-2023-20231020123601-150x150.jpg" alt="Leo (Telugu) (Original Motion Picture Soundtrack)" />
-                                <span><p>Leo (Telugu) (Original Motion Picture Soundtrack)</p>
-                                    <p>Telugu Album · 2023 · Anirudh Ravichander</p></span>
-                            </div>
-                            <div className="suggestions-card">
-                                <img src="https://c.saavncdn.com/109/Leo-Das-Entry-From-Leo-Tamil-2024-20240108151125-150x150.jpg" alt="Leo Das Entry (From 'Leo')" />
-                                <span> <p>Leo Das Entry (From "Leo")</p>
-                                    <p>Tamil Song · Leo Das Entry (From "Leo")</p></span>
-                            </div>
-                        </div>
-                        <div className="suggestions-box">
-                            <div className="suggestions-header">
-                                <h2>Top Result</h2>
-                                <button className="view-all-button">View All</button>
-                            </div>
-                            <div className="suggestions-card">
-                                <img src="https://c.saavncdn.com/415/Leo-Original-Motion-Picture-Soundtrack-English-2023-20231019170311-150x150.jpg" alt="Leo (Original Motion Picture Soundtrack)" />
-                                <span><p>Leo (Original Motion Picture Soundtrack)</p>
-                                    <p>Tamil Album · 2023 · Anirudh Ravichander</p></span>
-                            </div>
-                            <div className="suggestions-card">
-                                <img src="https://c.saavncdn.com/046/Leo-Telugu-Original-Motion-Picture-Soundtrack-English-2023-20231020123601-150x150.jpg" alt="Leo (Telugu) (Original Motion Picture Soundtrack)" />
-                                <span><p>Leo (Telugu) (Original Motion Picture Soundtrack)</p>
-                                    <p>Telugu Album · 2023 · Anirudh Ravichander</p></span>
-                            </div>
-                            <div className="suggestions-card">
-                                <img src="https://c.saavncdn.com/109/Leo-Das-Entry-From-Leo-Tamil-2024-20240108151125-150x150.jpg" alt="Leo Das Entry (From 'Leo')" />
-                                <span> <p>Leo Das Entry (From "Leo")</p>
-                                    <p>Tamil Song · Leo Das Entry (From "Leo")</p></span>
-                            </div>
-                        </div>
-                        <div className="suggestions-box">
-                            <div className="suggestions-header">
-                                <h2>Top Result</h2>
-                                <button className="view-all-button">View All</button>
-                            </div>
-                            <div className="suggestions-card">
-                                <img src="https://c.saavncdn.com/415/Leo-Original-Motion-Picture-Soundtrack-English-2023-20231019170311-150x150.jpg" alt="Leo (Original Motion Picture Soundtrack)" />
-                                <span><p>Leo (Original Motion Picture Soundtrack)</p>
-                                    <p>Tamil Album · 2023 · Anirudh Ravichander</p></span>
-                            </div>
-                            <div className="suggestions-card">
-                                <img src="https://c.saavncdn.com/046/Leo-Telugu-Original-Motion-Picture-Soundtrack-English-2023-20231020123601-150x150.jpg" alt="Leo (Telugu) (Original Motion Picture Soundtrack)" />
-                                <span><p>Leo (Telugu) (Original Motion Picture Soundtrack)</p>
-                                    <p>Telugu Album · 2023 · Anirudh Ravichander</p></span>
-                            </div>
-                            <div className="suggestions-card">
-                                <img src="https://c.saavncdn.com/109/Leo-Das-Entry-From-Leo-Tamil-2024-20240108151125-150x150.jpg" alt="Leo Das Entry (From 'Leo')" />
-                                <span> <p>Leo Das Entry (From "Leo")</p>
-                                    <p>Tamil Song · Leo Das Entry (From "Leo")</p></span>
-                            </div>
-                        </div>
-                        <div className="suggestions-box">
-                            <div className="suggestions-header">
-                                <h2>Top Result</h2>
-                                <button className="view-all-button">View All</button>
-                            </div>
-                            <div className="suggestions-card">
-                                <img src="https://c.saavncdn.com/415/Leo-Original-Motion-Picture-Soundtrack-English-2023-20231019170311-150x150.jpg" alt="Leo (Original Motion Picture Soundtrack)" />
-                                <span><p>Leo (Original Motion Picture Soundtrack)</p>
-                                    <p>Tamil Album · 2023 · Anirudh Ravichander</p></span>
-                            </div>
-                            <div className="suggestions-card">
-                                <img src="https://c.saavncdn.com/046/Leo-Telugu-Original-Motion-Picture-Soundtrack-English-2023-20231020123601-150x150.jpg" alt="Leo (Telugu) (Original Motion Picture Soundtrack)" />
-                                <span><p>Leo (Telugu) (Original Motion Picture Soundtrack)</p>
-                                    <p>Telugu Album · 2023 · Anirudh Ravichander</p></span>
-                            </div>
-                            <div className="suggestions-card">
-                                <img src="https://c.saavncdn.com/109/Leo-Das-Entry-From-Leo-Tamil-2024-20240108151125-150x150.jpg" alt="Leo Das Entry (From 'Leo')" />
-                                <span> <p>Leo Das Entry (From "Leo")</p>
-                                    <p>Tamil Song · Leo Das Entry (From "Leo")</p></span>
-                            </div>
-                        </div>
-
-                        <div className="suggestions-box">
-                            <h2>ALBUMS</h2>
-                            <div className="suggestions-card">
-                                <img src="https://c.saavncdn.com/415/Leo-Original-Motion-Picture-Soundtrack-English-2023-20231019170311-150x150.jpg" alt="Leo (Original Motion Picture Soundtrack)" />
-                                <span><p>Leo (Original Motion Picture Soundtrack)</p>
-                                    <p>Tamil Album · 2023 · Anirudh Ravichander</p></span>
-                            </div>
-                            <div className="suggestions-card">
-                                <img src="https://c.saavncdn.com/639/Leo-Punjabi-2024-20240103121125-150x150.jpg" alt="Leo" />
-                                <span><p>Leo</p>
-                                    <p>Punjabi Album · 2024 · Shubh</p></span>
-                            </div>
-                            <div className="suggestions-card">
-                                <img src="https://c.saavncdn.com/046/Leo-Telugu-Original-Motion-Picture-Soundtrack-English-2023-20231020123601-150x150.jpg" alt="Leo (Telugu) (Original Motion Picture Soundtrack)" />
-                                <span><p>Leo (Telugu) (Original Motion Picture Soundtrack)</p>
-                                    <p>Telugu Album · 2023 · Anirudh Ravichander</p></span>
-                            </div>
-                        </div>
-
-                        <div className="suggestions-box">
-                            <h2>SONGS</h2>
-                            <div className="suggestions-card">
-                                <img src="https://c.saavncdn.com/109/Leo-Das-Entry-From-Leo-Tamil-2024-20240108151125-150x150.jpg" alt="Leo Das Entry (From 'Leo')" />
-                                <span><p>Leo Das Entry (From "Leo")</p>
-                                    <p>Tamil Song · Leo Das Entry (From "Leo")</p></span>
-                            </div>
-                            <div className="suggestions-card">
-                                <img src="https://c.saavncdn.com/916/Ordinary-Person-From-Leo-Tamil-2023-20231023221744-150x150.jpg" alt="Ordinary Person (From 'Leo')" />
-                                <span><p>Ordinary Person (From "Leo")</p>
-                                    <p>Tamil Song · Ordinary Person (From "Leo")</p></span>
-                            </div>
-                            <div className="suggestions-card">
-                                <img src="https://c.saavncdn.com/435/Leo-Hindi-2021-20210817104943-150x150.jpg" alt="Leo" />
-                                <span><p>Leo</p>
-                                    <p>Hindi Song · Leo · Surmani Agni Verma</p></span>
-                            </div>
-                        </div>
+                        {Object.keys(autocompleteData).map(key => {
+                            if (autocompleteData[key].data.length > 0) {
+                                return (<div className="suggestions-box" key={key}>
+                                    <div className="suggestions-header">
+                                        <h3>{key}</h3>
+                                        <button className="view-all-button">View All</button>
+                                    </div>
+                                    {autocompleteData[key].data.slice(0, 3).map((value, index) => ( // Limit to 3 items
+                                        <div className="suggestions-card" key={index}>
+                                            <img src={value.image} alt={value.title} />
+                                            <span>
+                                                <p>{value.title}</p>
+                                                <p>Tamil Album · 2023 · Anirudh Ravichander</p>
+                                            </span>
+                                        </div>
+                                    ))}
+                                </div>)
+                            }
+                            return null;
+                        })}
                     </div>
+                    : (
+                        <p className="no-results">No results found</p>
+                    )}
 
-                ) : (
-                    <p className="no-results">No results found</p>
-                )}
             </div>
         </div>
     );
